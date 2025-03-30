da628e94b84e86aff7653697d0ac57f53495caed
William Kimandu <Hecubaster@gmail.com>
2025-03-30 17:28:24 +0200

    Add PHPUnit configuration and basic tests

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testBasicFeature()
+    {
+        $this->assertTrue(true);
+    }
+}
